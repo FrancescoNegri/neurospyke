9c7e6ebd97a08000952ff6100f621d801a1daa69
FrancescoNegri <francesco.negri@outlook.com>
2022-03-25 19:11:52 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.1.0'
+PACKAGE_NAME = 'neurospyke'
+AUTHOR = 'Francesco Negri'
+AUTHOR_EMAIL = 'francesco.negri@outlook.com'
+URL = 'https://github.com/FrancescoNegri/neurospyke'
+
+LICENSE = 'MIT License'
+DESCRIPTION = 'A work-in-progress Python3 library for neural signal analysis and visualization.'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      'numpy',
+      'pandas'
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages()
+      )
