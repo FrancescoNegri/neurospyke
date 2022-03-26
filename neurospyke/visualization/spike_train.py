6f706be4f283cee2b2db5ce72ef5bb4c0677c1b0
FrancescoNegri <francesco.negri@outlook.com>
2022-03-26 08:47:19 +0100

    Add kwargs in spike train plots

--- neurospyke/visualization/spike_train.py
+++ neurospyke/visualization/spike_train.py
@@ -1,7 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def _parse_parameters(spikes_idxs, sampling_time, channel_labels, channel_height):
+def _parse_kwargs(n_channels, **kwargs):
+    if 'channel_height' not in kwargs:
+        kwargs['channel_height'] = 0.5
+    if 'dpi' not in kwargs:
+        kwargs['dpi'] = 300
+    if 'figsize' not in kwargs:
+        kwargs['figsize'] = (16, kwargs.get('channel_height') * n_channels)
+    if 'linewidth' not in kwargs:
+        kwargs['linewidth'] = 0.5
+    if 'vertical_spacing' not in kwargs:
+        kwargs['vertical_spacing'] = 0.25
+
+    return kwargs
+
+def _parse_parameters(spikes_idxs, sampling_time, channel_labels):
     if type(spikes_idxs) is not list:
         spikes_idxs = [spikes_idxs]
     spikes_idxs = np.array(spikes_idxs, dtype=object)
@@ -9,23 +23,24 @@ def _parse_parameters(spikes_idxs, sampling_time, channel_labels, channel_height
     if (channel_labels is not None) and (type(channel_labels) is not list):
         channel_labels = [channel_labels]
 
-    return spikes_idxs, sampling_time, channel_labels, channel_height
+    return spikes_idxs, sampling_time, channel_labels
 
-def plot_spike_train(spikes_idxs, sampling_time, channel_labels=None, channel_height=0.5, plot_title='Spike Train'):    
-    spikes_idxs, sampling_time, channel_labels, channel_height = _parse_parameters(spikes_idxs, sampling_time, channel_labels, channel_height)
-
+def plot_spike_train(spikes_idxs, sampling_time, channel_labels=None, plot_title='Spike Train', **kwargs):    
+    spikes_idxs, sampling_time, channel_labels = _parse_parameters(spikes_idxs, sampling_time, channel_labels)
     n_channels = spikes_idxs.shape[0]
 
-    fig_height = channel_height * n_channels
-    plt.figure(figsize=(16, fig_height), dpi=300)
+    kwargs = _parse_kwargs(n_channels, **kwargs)
+    channel_height = kwargs.get('channel_height')
+
+    plt.figure(figsize=kwargs.get('figsize'), dpi=kwargs.get('dpi'))
 
     spikes_times = sampling_time * spikes_idxs
     
     for channel_idx in np.arange(n_channels):
         for spike_idx in np.arange(len(spikes_idxs[channel_idx])):
-            y = [channel_idx * channel_height, channel_idx * channel_height + channel_height * 0.75]
+            y = [channel_idx * channel_height, channel_idx * channel_height + channel_height * (1 - kwargs.get('vertical_spacing'))]
             x = [spikes_times[channel_idx][spike_idx], spikes_times[channel_idx][spike_idx]]
-            plt.plot(x, y, color='black', linewidth=0.5)
+            plt.plot(x, y, color='black', linewidth=kwargs.get('linewidth'))
 
     plt.title(plot_title)
     plt.xlabel('Time (s)')
