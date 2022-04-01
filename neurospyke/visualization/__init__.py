4c8d0fba80a104bf59083274236e9a2e583df006
FrancescoNegri <francesco.negri@outlook.com>
2022-04-01 19:33:51 +0200

    Add ISI histogram

--- neurospyke/visualization/__init__.py
+++ neurospyke/visualization/__init__.py
@@ -2,5 +2,6 @@ from .raw_data import plot_raw_data
 from .spikes import plot_spikes
 from .butterfly import plot_butterfly
 from .spike_train import plot_spike_train
+from .isi_hist import plot_ISI_hist
 
-__all__ = ['plot_raw_data', 'plot_spikes', 'plot_butterfly', 'plot_spike_train']
+__all__ = ['plot_raw_data', 'plot_spikes', 'plot_butterfly', 'plot_spike_train', 'plot_ISI_hist']
---
+++ neurospyke/visualization/isi_hist.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_ISI_hist(ISI, bins, plot_title='ISI Histogram', range=None):
+    plt.figure(figsize=(10, 5))
+    
+    if range is not None:
+        range = (range[0], range[1])
+
+    print(range)
+    plt.hist(ISI, bins=bins, range=range)
+    
+    plt.title(plot_title)
+    plt.xlabel('ISI (s)')
+    plt.ylabel('Spikes Count')
+
+    return
