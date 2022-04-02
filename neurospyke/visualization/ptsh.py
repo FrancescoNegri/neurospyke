bddcce9ccfb8ce10ffc96c01c944c72d083535d5
FrancescoNegri <francesco.negri@outlook.com>
2022-04-02 22:51:51 +0200

    Fix PTSH plot xticklabels

--- neurospyke/visualization/ptsh.py
+++ neurospyke/visualization/ptsh.py
@@ -1,9 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_PTSH(spikes_count, sampling_time, is_barplot=False, plot_title='PTSH Histogram'):
+def plot_PTSH(spikes_count, window_length, is_barplot=False, plot_title='PTSH Histogram'):
     plt.figure(figsize=(10, 5))
-    sampling_time = sampling_time * 1000
     bins = np.size(spikes_count, axis=0)
     
     if is_barplot is True:
@@ -13,7 +12,7 @@ def plot_PTSH(spikes_count, sampling_time, is_barplot=False, plot_title='PTSH Hi
         plt.plot(spikes_count, linewidth=2)
 
     plt.title(plot_title)
-    plt.xlabel('Time from stimulus (ms)')
+    plt.xlabel('Time from stimulus (s)')
     plt.ylabel('Spikes Count')
 
     ax = plt.gca()
@@ -21,8 +20,8 @@ def plot_PTSH(spikes_count, sampling_time, is_barplot=False, plot_title='PTSH Hi
     ax.set_ylim(0, None)
 
     xticks = np.array(ax.get_xticks())
-    xticklabels = np.array([np.round(label * sampling_time, 5) for label in xticks])
     xticks_idxs = np.where(ax.get_xticks() <= bins)
+    xticklabels = np.array([np.round(label * window_length / bins, 5) for label in xticks])
     ax.set_xticks(xticks[xticks_idxs])
     ax.set_xticklabels(xticklabels[xticks_idxs])
 
