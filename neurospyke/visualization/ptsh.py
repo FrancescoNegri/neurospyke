b91161acf1637a8f1b5cb018844f5cf4a6c57bec
FrancescoNegri <francesco.negri@outlook.com>
2022-04-03 09:05:58 +0200

    Add kwargs in PTSH plot

--- neurospyke/visualization/ptsh.py
+++ neurospyke/visualization/ptsh.py
@@ -1,17 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from .. import utils
 
-def plot_PTSH(spikes_count, window_length, is_barplot=False, plot_title='PTSH Histogram'):
-    plt.figure(figsize=(10, 5))
+def _parse_kwargs(**kwargs):
+    kwargs_list = [
+        {'key': 'alpha', 'default': 0.25, 'type': float},
+        {'key': 'color', 'default': '#1f77b4', 'type': str},
+        {'key': 'dpi', 'default': 100, 'type': float},
+        {'key': 'figsize', 'default': (6, 3), 'type': tuple},
+        {'key': 'is_barplot', 'default': False, 'type': bool},
+        {'key': 'linewidth', 'default': 2, 'type': float},
+        {'key': 'plot_title', 'default': 'PTSH Histogram', 'type': str}
+    ]
+    kwargs = utils.check_kwargs_list(kwargs_list, **kwargs)
+
+    return kwargs
+
+
+def plot_PTSH(spikes_count, window_length, **kwargs):
+    kwargs = _parse_kwargs(**kwargs)
+
+    plt.figure(figsize=kwargs.get('figsize'), dpi=kwargs.get('dpi'))
     bins = np.size(spikes_count, axis=0)
     
-    if is_barplot is True:
-        plt.bar(np.arange(bins), spikes_count, align='edge')
+    if kwargs.get('is_barplot') is True:
+        plt.bar(np.arange(bins), spikes_count, align='edge', color=kwargs.get('color'))
     else:
-        plt.fill_between(np.arange(bins), spikes_count, alpha=0.25)
-        plt.plot(spikes_count, linewidth=2)
+        plt.fill_between(np.arange(bins), spikes_count, facecolor=kwargs.get('color'), alpha=kwargs.get('alpha'))
+        plt.plot(spikes_count, linewidth=kwargs.get('linewidth'), color=kwargs.get('color'))
 
-    plt.title(plot_title)
+    plt.title(kwargs.get('plot_title'))
     plt.xlabel('Time from stimulus (s)')
     plt.ylabel('Spikes Count')
 
