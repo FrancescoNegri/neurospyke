3bbee9ebfd0659dd46879f42a4e3ed0f98d84895
FrancescoNegri <francesco.negri@outlook.com>
2022-04-05 23:42:00 +0200

    Add kwargs to butterfly plot

--- neurospyke/visualization/butterfly.py
+++ neurospyke/visualization/butterfly.py
@@ -1,18 +1,34 @@
-import numpy as np
 import math
+import numpy as np
 import matplotlib.pyplot as plt
+from .. import utils
 
-def plot_butterfly(data, sampling_time, spikes_idxs, window_half_length, plot_title='Butterfly Plot'):
-    plt.figure(figsize=(10, 5))
-    window_half_length_idx = math.floor(window_half_length/sampling_time)
-    window_times = sampling_time*np.arange(-window_half_length_idx, window_half_length_idx, 1)
+def _parse_kwargs(**kwargs):
+    kwargs_list = [
+        {'key': 'dpi', 'default': 100, 'type': float},
+        {'key': 'figsize', 'default': (6, 3), 'type': tuple},
+        {'key': 'linewidth', 'default': 0.5, 'type': float},
+        {'key': 'title', 'default': 'Butterfly Plot', 'type': str},
+        {'key': 'window_length', 'default': 0.001, 'type': float}
+    ]
+    kwargs = utils.check_kwargs_list(kwargs_list, **kwargs)
+
+    return kwargs
+
+def plot_butterfly(data, sampling_time, spikes_idxs, **kwargs):
+    kwargs = _parse_kwargs(**kwargs)
+
+    plt.figure(figsize=kwargs.get('figsize'), dpi=kwargs.get('dpi'))
+    window_half_length = kwargs.get('window_length') / 2
+    window_half_length_idx = math.floor(window_half_length / sampling_time)
+    window_times = sampling_time * 1000 * np.arange(-window_half_length_idx, window_half_length_idx, 1)
     
     for i in range(len(spikes_idxs)):
-        window_data = data[range(spikes_idxs[i]-window_half_length_idx, spikes_idxs[i]+window_half_length_idx, 1)]
-        plt.plot(window_times, window_data, linewidth=0.5)
+        window_data = data[range(spikes_idxs[i] - window_half_length_idx, spikes_idxs[i] + window_half_length_idx, 1)]
+        plt.plot(window_times, window_data, linewidth=kwargs.get('linewidth'))
 
-    plt.title(plot_title)
-    plt.xlabel('Time distance from spike (s)')
+    plt.title(kwargs.get('title'))
+    plt.xlabel('Time distance from spike (ms)')
     plt.ylabel('Voltage (µV)')
 
     ax = plt.gca()
