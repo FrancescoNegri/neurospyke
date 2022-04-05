3d1a5a638578dd6aaafbbe7c3fa04b50fc25fb45
FrancescoNegri <francesco.negri@outlook.com>
2022-04-05 23:28:55 +0200

    Add kwargs to spike plot

--- neurospyke/visualization/spikes.py
+++ neurospyke/visualization/spikes.py
@@ -1,15 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from .. import utils
 
-def plot_spikes(data, sampling_time, spikes_idxs, plot_title='Spike Plot'):
-    plt.figure(figsize=(10, 5))
-    times = sampling_time*np.arange(0, len(data), 1)
-    spikes_times = sampling_time*spikes_idxs
+def _parse_kwargs(**kwargs):
+    kwargs_list = [
+        {'key': 'color', 'default': '#1f77b4', 'type': str},
+        {'key': 'dpi', 'default': 100, 'type': float},
+        {'key': 'figsize', 'default': (6, 3), 'type': tuple},
+        {'key': 'linewidth', 'default': 0.25, 'type': float},
+        {'key': 'marker', 'default': '*', 'type': str},
+        {'key': 'markercolor', 'default': 'red', 'type': str},
+        {'key': 'markersize', 'default': 2, 'type': float},
+        {'key': 'title', 'default': 'Spike Plot', 'type': str}
+    ]
+    kwargs = utils.check_kwargs_list(kwargs_list, **kwargs)
+
+    return kwargs
+
+def plot_spikes(data, sampling_time, spikes_idxs, **kwargs):
+    kwargs = _parse_kwargs(**kwargs)
+
+    plt.figure(figsize=kwargs.get('figsize'), dpi=kwargs.get('dpi'))
+    times = sampling_time * np.arange(0, len(data), 1)
+    spikes_times = sampling_time * spikes_idxs
     
-    plt.plot(times, data, linewidth=0.25)
-    plt.plot(spikes_times, data[spikes_idxs], color='red', marker='*', markersize=3, linestyle='None')
+    plt.plot(times, data, linewidth=kwargs.get('linewidth'), color=kwargs.get('color'))
+    plt.plot(spikes_times, data[spikes_idxs], color=kwargs.get('markercolor'), marker=kwargs.get('marker'), markersize=kwargs.get('markersize'), linestyle='None')
 
-    plt.title(plot_title)
+    plt.title(kwargs.get('title'))
     plt.xlabel('Time (s)')
     plt.ylabel('Voltage (µV)')
 
