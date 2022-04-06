0c6c1de9c05a78d15713cbab5465ec9171dafcfa
FrancescoNegri <francesco.negri@outlook.com>
2022-04-07 00:10:39 +0200

    Update kwargs in spike train plot

--- neurospyke/visualization/spike_train.py
+++ neurospyke/visualization/spike_train.py
@@ -1,58 +1,55 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+
 from .. import utils
 
 def _parse_kwargs(spikes_times, n_channels, **kwargs):
     # Basic checks
     kwargs_list = [
+        {'key': 'boxoff', 'default': True, 'type': bool},
         {'key': 'channel_height', 'default': 0.5, 'type': float},
+        {'key': 'channel_labels', 'default': [str(i) for i in np.arange(1, n_channels + 1, 1)], 'type': tuple},
         {'key': 'color', 'default': 'black', 'type': str},
         {'key': 'dpi', 'default': 300, 'type': float},
+        {'key': 'figsize', 'default': (16, kwargs.get('channel_height', 0.5) * n_channels), 'type': tuple},
         {'key': 'linewidth', 'default': 0.5, 'type': float},
+        {'key': 'num', 'default': None, 'type': str},
+        {'key': 'sampling_time', 'default': None, 'type': float},
         {'key': 'title', 'default': 'Spike Train', 'type': str},
         {'key': 'reverse', 'default': False, 'type': bool},
         {'key': 'vertical_spacing', 'default': 0.25, 'type': float},
-        {'key': 'xlim', 'default': (0, np.amax([spikes_times[channel_idx][-1] for channel_idx in np.arange(n_channels)]) * 1.01), 'type': tuple}
+        {'key': 'xlabel', 'default': 'Time (s)' if kwargs.get('sampling_time', None) is not None else 'Samples', 'type': str},
+        {'key': 'xlim', 'default': (0, np.amax([spikes_times[channel_idx][-1] for channel_idx in np.arange(n_channels)]) * 1.01), 'type': tuple},
+        {'key': 'ylabel', 'default': 'Channels', 'type': str}
     ]
     kwargs = utils.check_kwargs_list(kwargs_list, **kwargs)
 
     # Additional checks
     if kwargs.get('vertical_spacing') < 0 or kwargs.get('vertical_spacing') > 1:
         raise ValueError("'vertical_spacing' expected to be a value between 0 and 1, received " + str(kwargs.get('vertical_spacing')))
-    
-    # Dependant checks
-    kwargs_list = [
-        {'key': 'figsize', 'default': (16, kwargs.get('channel_height') * n_channels), 'type': tuple}
-    ]
-    kwargs = utils.check_kwargs_list(kwargs_list, **kwargs)
+    if len(kwargs.get('channel_labels')) != n_channels:
+        raise ValueError("'channel_labels' expected to contain " + str(n_channels) + " elements, received " + str(len(kwargs.get('channel_labels'))))
     
     return kwargs
 
-def _parse_parameters(spikes_idxs, sampling_time, channel_labels):
-    if type(spikes_idxs) is not list:
-        spikes_idxs = [spikes_idxs]
+def plot_spike_train(spikes_idxs, **kwargs):
+    if ((type(spikes_idxs) is np.ndarray) and (len(np.shape(spikes_idxs)))) or (len(spikes_idxs) == 1):
+        if (hasattr(spikes_idxs[0], '__iter__') is False):
+            spikes_idxs = [spikes_idxs]        
+    
     spikes_idxs = np.array(spikes_idxs, dtype=object)
 
-    if (channel_labels is not None) and (type(channel_labels) is not list):
-        channel_labels = [channel_labels]
-
-    return spikes_idxs, sampling_time, channel_labels
-
-def plot_spike_train(spikes_idxs, sampling_time, channel_labels=None, **kwargs):    
-    spikes_idxs, sampling_time, channel_labels = _parse_parameters(spikes_idxs, sampling_time, channel_labels)
     n_channels = spikes_idxs.shape[0]
-    spikes_times = sampling_time * spikes_idxs
+    spikes_times = kwargs.get('sampling_time') * spikes_idxs if kwargs.get('sampling_time') is not None else spikes_idxs
 
     kwargs = _parse_kwargs(spikes_times, n_channels, **kwargs)
 
-    plt.figure(figsize=kwargs.get('figsize'), dpi=kwargs.get('dpi'))
-
-    if (channel_labels is None) or (len(channel_labels) != n_channels):
-        channel_labels = [str(i) for i in np.arange(1, n_channels + 1, 1)]
+    plt.figure(num=kwargs.get('num'), figsize=kwargs.get('figsize'), dpi=kwargs.get('dpi'))
 
     if (kwargs.get('reverse') is True) and (n_channels > 1):
         spikes_times = np.flip(spikes_times, axis=0)
-        channel_labels = np.flip(channel_labels)
+        kwargs['channel_labels'] = np.flip(kwargs.get('channel_labels'))
     
     for channel_idx in np.arange(n_channels):
         for spike_idx in np.arange(len(spikes_times[channel_idx])):
@@ -61,21 +58,24 @@ def plot_spike_train(spikes_idxs, sampling_time, channel_labels=None, **kwargs):
             plt.plot(x, y, color=kwargs.get('color'), linewidth=kwargs.get('linewidth'))
 
     plt.title(kwargs.get('title'))
-    plt.xlabel('Time (s)')
-    plt.ylabel('Channels')
+    plt.xlabel(kwargs.get('xlabel'))
+    plt.ylabel(kwargs.get('ylabel'))
 
     ax = plt.gca()
-    
-    ax.set_xlim(kwargs.get('xlim')[0], kwargs.get('xlim')[1])
+    ax.set_xlim(kwargs.get('xlim'))
     ax.set_ylim(0, kwargs.get('channel_height') * n_channels)
     
     yticks = np.arange(kwargs.get('channel_height') / 2, round(n_channels * kwargs.get('channel_height') + kwargs.get('channel_height') / 2, 8), kwargs.get('channel_height'))
     ax.set_yticks(yticks)
-    ax.set_yticklabels(channel_labels)
+    ax.set_yticklabels(kwargs.get('channel_labels'))
     ax.tick_params(axis='y', which='both', length=0)
+
+    if kwargs.get('sampling_time') is None:
+        ax.xaxis.set_major_locator(MaxNLocator(integer=True))
     
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.spines['left'].set_visible(False)
+    if kwargs.get('boxoff') is True:
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.spines['left'].set_visible(False)
 
     return
