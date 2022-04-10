c687962fcebe82db97869e129e917c419f8392ac
FrancescoNegri <francesco.negri@outlook.com>
2022-04-10 19:10:38 +0200

    Update kwargs in ptsh plot

--- neurospyke/visualization/ptsh.py
+++ neurospyke/visualization/ptsh.py
@@ -1,41 +1,52 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 from .. import utils
 
 def _parse_kwargs(**kwargs):
     kwargs_list = [
         {'key': 'alpha', 'default': 0.25, 'type': float},
+        {'key': 'boxoff', 'default': True, 'type': bool},
         {'key': 'color', 'default': '#1f77b4', 'type': str},
         {'key': 'dpi', 'default': 100, 'type': float},
         {'key': 'figsize', 'default': (6, 3), 'type': tuple},
-        {'key': 'is_barplot', 'default': False, 'type': bool},
+        {'key': 'barplot', 'default': False, 'type': bool},
         {'key': 'linewidth', 'default': 2, 'type': float},
-        {'key': 'title', 'default': 'PTSH Histogram', 'type': str}
+        {'key': 'normalize', 'default': False, 'type': bool},
+        {'key': 'num', 'default': None, 'type': str},
+        {'key': 'sampling_time', 'default': None, 'type': float},
+        {'key': 'title', 'default': 'PTSH Histogram', 'type': str},
+        {'key': 'xlabel', 'default': 'Time from stimulus (s)' if kwargs.get('sampling_time', None) is not None else 'Samples from stimulus', 'type': str},
+        {'key': 'xlim', 'default': (0, None), 'type': tuple},
+        {'key': 'ylabel', 'default': 'Spikes Count', 'type': str},
+        {'key': 'ylim', 'default': (0, None), 'type': tuple}
     ]
     kwargs = utils.check_kwargs_list(kwargs_list, **kwargs)
 
     return kwargs
 
-
 def plot_PTSH(spikes_count, window_length, **kwargs):
     kwargs = _parse_kwargs(**kwargs)
 
-    plt.figure(figsize=kwargs.get('figsize'), dpi=kwargs.get('dpi'))
+    plt.figure(num=kwargs.get('num'), figsize=kwargs.get('figsize'), dpi=kwargs.get('dpi'))
     bins = np.size(spikes_count, axis=0)
+
+    if kwargs.get('normalize') is True:
+        spikes_count = spikes_count/np.sum(spikes_count)
     
-    if kwargs.get('is_barplot') is True:
+    if kwargs.get('barplot') is True:
         plt.bar(np.arange(bins), spikes_count, align='edge', color=kwargs.get('color'))
     else:
         plt.fill_between(np.arange(bins), spikes_count, facecolor=kwargs.get('color'), alpha=kwargs.get('alpha'))
         plt.plot(spikes_count, linewidth=kwargs.get('linewidth'), color=kwargs.get('color'))
 
     plt.title(kwargs.get('title'))
-    plt.xlabel('Time from stimulus (s)')
-    plt.ylabel('Spikes Count')
+    plt.xlabel(kwargs.get('xlabel'))
+    plt.ylabel(kwargs.get('ylabel'))
 
     ax = plt.gca()
-    ax.set_xlim(0, None)
-    ax.set_ylim(0, None)
+    ax.set_xlim(kwargs.get('xlim'))
+    ax.set_ylim(kwargs.get('ylim'))
 
     xticks = np.array(ax.get_xticks())
     xticks_idxs = np.where(ax.get_xticks() <= bins)
@@ -43,7 +54,11 @@ def plot_PTSH(spikes_count, window_length, **kwargs):
     ax.set_xticks(xticks[xticks_idxs])
     ax.set_xticklabels(xticklabels[xticks_idxs])
 
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
+    if kwargs.get('sampling_time') is None:
+        ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+
+    if kwargs.get('boxoff') is True:
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
     
     return
