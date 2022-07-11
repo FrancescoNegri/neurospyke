6598daee06d53fc518dc0cf108eb8c1a4fb45e77
FrancescoNegri <francesco.negri@outlook.com>
2022-07-11 11:30:51 +0200

    Fix raster subplots

--- neurospyke/visualization/raster.py
+++ neurospyke/visualization/raster.py
@@ -3,16 +3,16 @@ import matplotlib.pyplot as plt
 from .. import utils
 from .spike_train import plot_spike_train
 
-def plot_raster(spikes_obj, sampling_time, n_cols, is_train = True):
+def plot_raster(spikes_obj, sampling_time, n_cols = 1, is_train = True, figsize=[12, 8]):
     if np.size(np.shape(spikes_obj)) == 2:
         spikes_obj = np.array([spikes_obj])
     
     n_channels = np.size(spikes_obj, 0)
     n_rows = int(np.ceil(n_channels/n_cols))
 
-    fig, axs = plt.subplot(n_rows, n_cols, figsize=[12,8], constrained_layout=True)
+    fig, axs = plt.subplots(n_rows, n_cols, figsize=figsize, squeeze=False, constrained_layout=True)
 
-    for channel_idx in n_channels:
+    for channel_idx in np.arange(n_channels):
         spikes_matrix = spikes_obj[channel_idx]
         spikes_idxs = []
         for trial_idx in np.arange(np.size(spikes_matrix, 0)):
@@ -22,7 +22,7 @@ def plot_raster(spikes_obj, sampling_time, n_cols, is_train = True):
                 spikes_idxs.append(spikes_matrix[trial_idx])
 
         row = int(np.floor(channel_idx / n_cols))
-        col = trial_idx % n_cols
+        col = channel_idx % n_cols
 
-        plot_spike_train(spikes_idxs, ax=axs[row,col], sampling_time=sampling_time, figsize=(6, 5), dpi=75, ylabel='Trials', reverse=True, title='Stimulus-Related Raster Plot - Channel #' + str(channel_idx))
+        plot_spike_train(spikes_idxs, ax=axs[row, col], sampling_time=sampling_time, figsize=[figsize[0] / n_cols, figsize[1] / n_rows], dpi=75, ylabel='Trials', channel_labels=None, reverse=True, title='Channel #' + str(channel_idx))
     fig.suptitle('Stimulus-Related Raster Plots')
