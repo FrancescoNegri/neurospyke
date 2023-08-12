58b71d9ae86d44ff25f777f86a4d6c954176ded5
FrancescoNegri <francesco.negri@outlook.com>
2023-08-12 17:06:42 -0500

    Update raw data plots

--- neurospyke/visualization/raw_data.py
+++ neurospyke/visualization/raw_data.py
@@ -1,5 +1,6 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+
 from matplotlib.ticker import MaxNLocator
 from .. import utils
 
@@ -23,8 +24,11 @@ def _parse_kwargs(**kwargs):
 
     return kwargs
 
-def plot_raw_data(data, **kwargs):
+def plot_raw_data(data:np.ndarray, **kwargs):
     kwargs = _parse_kwargs(**kwargs)
+
+    # Cast data type to float
+    data = data.astype(np.float64).squeeze()
 
     if kwargs.get('ax') is None:
         plt.figure(num=kwargs.get('num'), figsize=kwargs.get('figsize'), dpi=kwargs.get('dpi'))
@@ -32,7 +36,7 @@ def plot_raw_data(data, **kwargs):
     else:
         ax = kwargs.get('ax')
 
-    times = kwargs.get('sampling_time') * np.arange(0, len(data), 1) if kwargs.get('sampling_time') is not None else np.arange(0, len(data), 1)
+    times = kwargs.get('sampling_time') * np.arange(0, np.size(data), 1) if kwargs.get('sampling_time') is not None else np.arange(0, np.size(data), 1)
     
     ax.plot(times, data, linewidth=kwargs.get('linewidth'), color=kwargs.get('color'))
 
@@ -42,7 +46,6 @@ def plot_raw_data(data, **kwargs):
     
     ax.set_xlim(0, times[-1]) if kwargs.get('xlim') is None else ax.set_xlim(kwargs.get('xlim'))
     ax.set_ylim(None, None) if kwargs.get('ylim') is None else ax.set_ylim(kwargs.get('ylim'))
-
 
     if kwargs.get('sampling_time') is None:
         ax.xaxis.set_major_locator(MaxNLocator(integer=True))
