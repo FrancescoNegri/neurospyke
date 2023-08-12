f5555ee33c8ef9738ae5753a5d64dc987660d447
FrancescoNegri <francesco.negri@outlook.com>
2023-08-11 23:22:17 -0500

    Update hard threshold spike detection

--- neurospyke/spikes/detection/__init__.py
+++ neurospyke/spikes/detection/__init__.py
@@ -1,11 +1,11 @@
-from .hard import hard_threshold_local_maxima
+from .hard import hard_threshold
 from .differential import differential_threshold
 from .ptsd import PTSD
 from .swtteo import SWTTEO
 from .oswtteo import OSWTTEO
 
 __all__ = [
-        'hard_threshold_local_maxima',
+        'hard_threshold',
         'differential_threshold',
         'PTSD',
         'SWTTEO',
--- neurospyke/spikes/detection/hard.py
+++ neurospyke/spikes/detection/hard.py
@@ -5,20 +5,20 @@ from ... import utils
 def _parse_kwargs(**kwargs):
     kwargs_list = [
         {'key': 'sampling_time', 'default': None, 'type': float},
-        {'key': 'use_abs', 'default': False, 'type': bool}
+        {'key': 'polarity', 'default': -1, 'type': int}
     ]
     kwargs = utils.check_kwargs_list(kwargs_list, **kwargs)
 
     return kwargs
 
-def hard_threshold_local_maxima(data:np.ndarray, threshold:float, refractory_period:float, **kwargs):
+def hard_threshold(data:np.ndarray, threshold:float, refractory_period:float, **kwargs):
     '''
     Use the Hard Threshold Local Maxima algorithm to detect spikes,
     with parameters specified either in the time domain or in samples.
 
     Parameters
     ----------
-    data : numpy.ndarray
+    data : ndarray
         The array of recorded data.
     threshold : float
         A threshold employed by the algorithm to detect a spike.
@@ -28,19 +28,21 @@ def hard_threshold_local_maxima(data:np.ndarray, threshold:float, refractory_per
         The sampling time for the recorded data. If specified, the algorithm
         will work in the time domain (the other parameters should then be
         specified in seconds). Otherwise, it will work with samples.
-    use_abs : bool, defualt=False
-        A flag indicating if the absolute value of the data is to be used.
+    polarity : {-1, 0, 1}, defualt=-1
+        The polarity of spikes to look for. -1 means negative polarity,
+        1 mean positive ones, while 0 applies the absolute value to the
+        signal.
     
     Returns
     -------
-    spikes_idxs : numpy.ndarray
+    spikes_idxs : ndarray
         An array containing all the indices of detected spikes.
-    spikes_values numpy.ndarray
+    spikes_values ndarray
         An array containing all the values (i.e. amplitude) of detected spikes.
 
     References
     ----------
-    [1] S. Gibson et al. “Technology-Aware Algorithm Design for Neural Spike Detection, Feature Extraction, and Dimensionality Reduction.” IEEE Transactions on Neural Systems and Rehabilitation Engineering, vol. 18, no. 5, 2010, pp. 469-478., https://doi.org/10.1109/tnsre.2010.2051683
+    [1] Gibson, S., Judy, J. W., & Markovic, D. (2010). Technology-Aware Algorithm Design for Neural Spike Detection, Feature Extraction, and Dimensionality Reduction. IEEE Transactions on Neural Systems and Rehabilitation Engineering, 18(5), 469–478. https://doi.org/10.1109/tnsre.2010.2051683
     '''
     kwargs = _parse_kwargs(**kwargs)
     
@@ -48,15 +50,16 @@ def hard_threshold_local_maxima(data:np.ndarray, threshold:float, refractory_per
     refractory_period = utils.get_in_samples(refractory_period, kwargs.get('sampling_time'))
 
     # Cast data type to float
-    data = data.astype(np.float64)
-
-    if kwargs.get('use_abs') is True:
-        data = abs(data)
+    data = data.astype(np.float64).squeeze()
     
-    spikes_idxs, _ = find_peaks(data, height=threshold, distance=refractory_period)
-    spikes_values = data[spikes_idxs]
+    if kwargs.get('polarity') == -1:
+        spikes_idxs, _ = find_peaks(-data, height=-threshold, distance=refractory_period)
+    elif kwargs.get('polarity') == 0:
+        spikes_idxs, _ = find_peaks(abs(data), height=threshold, distance=refractory_period)
+    else:
+        spikes_idxs, _ = find_peaks(data, height=threshold, distance=refractory_period)
 
-    spikes_idxs = np.array(spikes_idxs, dtype=np.int64)
-    spikes_values = np.array(spikes_values, dtype=np.float64)
+    spikes_idxs = spikes_idxs.astype(np.int64)
+    spikes_values = data[spikes_idxs].astype(np.float64)
     
     return spikes_idxs, spikes_values
