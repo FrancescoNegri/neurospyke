f27939f7fd9f1ef5492bd5de1f1d2414f0ed2732
FrancescoNegri <francesco.negri@outlook.com>
2023-08-11 23:45:16 -0500

    Update differential threshold spike detection

--- neurospyke/spikes/detection/differential.py
+++ neurospyke/spikes/detection/differential.py
@@ -4,6 +4,7 @@ from ... import utils
 
 def _parse_kwargs(**kwargs):
     kwargs_list = [
+        {'key': 'polarity', 'default': -1, 'type': int},
         {'key': 'sampling_time', 'default': None, 'type': float}
     ]
     kwargs = utils.check_kwargs_list(kwargs_list, **kwargs)
@@ -18,7 +19,7 @@ def differential_threshold(data:np.ndarray, threshold:float, window_length:float
 
     Parameters
     ----------
-    data : numpy.ndarray
+    data : ndarray
         The array of recorded data.
     threshold : float
         A threshold employed by the algorithm to detect a spike.
@@ -27,6 +28,9 @@ def differential_threshold(data:np.ndarray, threshold:float, window_length:float
         for spikes to detect, expressed in seconds or samples.
     refractory_period : float
         The detection algorithm refractory period, expressed in seconds or samples.
+    polarity : {-1, 1}, defualt=-1
+        The polarity of spikes to look for. -1 means negative polarity,
+        1 mean positive ones.
     sampling_time : float, optional
         The sampling time for the recorded data. If specified, the algorithm
         will work in the time domain (the other parameters should then be
@@ -34,14 +38,14 @@ def differential_threshold(data:np.ndarray, threshold:float, window_length:float
     
     Returns
     -------
-    spikes_idxs : numpy.ndarray
+    spikes_idxs : ndarray
         An array containing all the indices of detected spikes.
-    spikes_values numpy.ndarray
+    spikes_values ndarray
         An array containing all the values (i.e. amplitude) of detected spikes.
     
     References
     ----------
-    [1] A. Maccione et al. “A novel algorithm for precise identification of spikes in extracellularly recorded neuronal signals.” Journal of neuroscience methods vol. 177,1 (2009): 241-9. https://doi.org/10.1016/j.jneumeth.2008.09.026
+    [1] Maccione, A., Gandolfo, M., Massobrio, P., Novellino, A., Martinoia, S., & Chiappalone, M. (2009). A novel algorithm for precise identification of spikes in extracellularly recorded neuronal signals. Journal of Neuroscience Methods, 177(1), 241–249. https://doi.org/10.1016/j.jneumeth.2008.09.026
     '''
     kwargs = _parse_kwargs(**kwargs)
     
@@ -50,12 +54,14 @@ def differential_threshold(data:np.ndarray, threshold:float, window_length:float
     refractory_period = utils.get_in_samples(refractory_period, kwargs.get('sampling_time'))
 
     # Cast data type to float
-    data = data.astype(np.float64)
+    data = data.astype(np.float64).squeeze()
+
+    if kwargs.get('polarity') == -1:
+        data = -data
 
     n_windows = math.floor(len(data) / window_length)
 
     spikes_idxs = []
-    spikes_values = []
 
     for i in range(n_windows):
         window_idx = window_length * i
@@ -66,12 +72,13 @@ def differential_threshold(data:np.ndarray, threshold:float, window_length:float
         if abs(max_value - min_value) >= threshold:
             if (len(spikes_idxs) != 0 and (np.argmax(window_data) + window_idx - spikes_idxs[-1]) > refractory_period):
                 spikes_idxs.append(np.argmax(window_data) + window_idx)
-                spikes_values.append(max_value)
             elif len(spikes_idxs) == 0:
                 spikes_idxs.append(np.argmax(window_data) + window_idx)
-                spikes_values.append(max_value)
+
+    if kwargs.get('polarity') == -1:
+        data = -data
 
     spikes_idxs = np.array(spikes_idxs, dtype=np.int64)
-    spikes_values = np.array(spikes_values, dtype=np.float64)
+    spikes_values = data[spikes_idxs].astype(np.float64)
     
     return spikes_idxs, spikes_values
